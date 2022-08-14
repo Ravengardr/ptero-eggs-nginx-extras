157081e33b4f7cea0d27dc3bbde20d9905f8def9
Ravengardr <111186399+Ravengardr@users.noreply.github.com>
2022-08-14 16:21:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ echo "Starting PHP-FPM..."
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "Starting Nginx..."
-echo "Successfully started"
-/usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
+/etc/nginx -c /home/container/nginx/nginx.conf -p /home/container/
+echo "Nginx successfully started"
