29733fad644fafe6f507cbd6de8481f9dd45363d
Ravengardr <111186399+Ravengardr@users.noreply.github.com>
2022-08-14 17:57:23 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ rm -rf /home/container/tmp/*
 mkdir /home/container/logs/
 
 echo "Starting PHP-FPM..."
-/usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+/usr/sbin/php-fpm8.0 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "Starting Nginx..."
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf
