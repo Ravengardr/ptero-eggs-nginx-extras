23bed37a0564c9d0a726dda5a6c9259b0823c2c7
Ravengardr <111186399+Ravengardr@users.noreply.github.com>
2022-08-14 02:07:35 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
 
+mkdir /home/container/logs/
+
 echo "Starting PHP-FPM..."
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
